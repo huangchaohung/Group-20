79ec2bd9aff7884ad55eca7b2f08b5f9103ac07a
huangchaohung <huangchaohung12@gmail.com>
2024-11-10 22:13:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,25 @@ if __name__ == "__main__":
     ### run something, show a plot here
     ### one paragraph answer the question
     print("---------------------------")
+    print("2. What are the key factors influencing customer engagement with our marketing campaigns?")
+    ### run something, show a plot here
+    ### one paragraph answer the question
+    print("---------------------------")
+    print("3. How do customer behaviors and preferences vary across different segments?")
+    ### run something, show a plot here
+    ### one paragraph answer the question
+    print("---------------------------")
     print("Group B")
     print("---------------------------")
-    ### Put all the Group B processing codes here
+    print("1. How can we accurately predict individual customer preferences and needs?")
+    ### run something, show a plot here
+    ### one paragraph answer the question
+    print("---------------------------")
+    print("2. What strategies can we implement to optimize our marketing campaigns in real-time?")
+    ### run something, show a plot here
+    ### one paragraph answer the question
+    print("---------------------------")
+    print("3. How can we measure and maximize the ROI of our personalized marketing efforts?")
+    ### run something, show a plot here
+    ### one paragraph answer the question
+    print("---------------------------")
